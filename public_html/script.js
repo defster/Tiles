e3326424505d64c9d2115380006a0f7d74b82970
Øyvind Husabø <ohusabo@gmail.com>
2015-02-25 02:26:45 +0100

    changes

--- public_html/script.js
+++ public_html/script.js
@@ -18,15 +18,16 @@ $(document).ready(function()
     var screenY = context.canvas.height;
     // Mouse position in worldspace.
     var mouse_x, mouse_y;
+    var pMouse_x, pMouse_y;
     var mouse_x_tile, mouse_y_tile;
 
-    var SHADOWS = true;
+    var SHADOWS = false;
     
     // This value will always be the index of the currently selected unit.
     var unit = 0;
     var paths = [[]];
     var speed = 2;
-    var tilesize = 32;
+    var tilesize = 24;
     var maxTilesX = (screenX / tilesize) | 0;
     var maxTilesY = (screenY / tilesize) | 0;
     var midx = (screenX / 2) | 0;
@@ -93,108 +94,6 @@ $(document).ready(function()
         $('#defaultSlider').change();
     });
 
-    // Where u1 attacks u2
-    function calcDamage(u1, u2)
-    {
-        // The damage bonus/penalty from your STR to enemy's VIT for melee attack.
-        var fStr = 0;
-        
-        // rank = weapon dps / 9 floored. weaponRank is -rank to +rank
-        var weaponRank = 0;
-        var fStrLowerCap = -weaponRank;
-        var fStrUpperCap = weaponRank + 8;
-        // 0 = str, 2 = vit. should make that array global..
-        fStr = units[u1].stats['str'] - units[u2].stats['vit'];
-        if (fStr < fStrLowerCap)
-            fStr = fStrLowerCap;
-        if (fStr > fStrUpperCap)
-            fStr = fStrUpperCap;
-        
-        // TODO: fStr2 for ranged attack
-        
-        // Calculate base damage
-        var baseDamage;
-        var pDIF = 1; // *
-        baseDamage = 5 + fStr; // HARDCODED
-        
-        // pDIF; attack vs defense difference
-        
-        var ratio = units[u1].stats['attack'] / units[u2].stats['defense'];
-        //console.log("ratio: ", ratio);
-        if (ratio > 2.25)
-            ratio = 2.25;
-        var levelDiff = units[u2].lvl - units[u1].lvl;
-        if (levelDiff < 0)
-            levelDiff = 0;
-        //console.log("levelDiff: ", levelDiff);
-        var cRatio = ratio - (0.05 * levelDiff);
-        
-        var r = cRatio;
-        //console.log("cRatio: ", r);
-        var b = 0;
-        if (0 <= r && r <= 0.5)
-            b = 1 + (10 / 9) * (r - 0.5);
-        if (0.5 <= r && r <= 3/4)
-            b = 1;
-        if (3/4 <= r && r <= 2.25)
-            b = 1 + (10 / 9) * (r - 3/4);
-        
-        //console.log("b max: ", b);
-        
-        var a = 0;
-        if (r <= 0.5)
-            a = 1/6;
-        if (0.5 <= r && r < 1.25)
-            a = 1 + (10 / 9) * (r - 1.25);
-        if (1.25 <= r && r <= 1.5)
-            a = 1;
-        if (1.5 < r && r <= 2.25)
-            a = 1 + (10 / 9) * (r - 1.5);
-        
-        //console.log("a min: ", a);
-        
-        var ra1 = 1/6;
-        var randomizer = b - a;
-        var ra2 = a + (Math.random() * randomizer);
-        ra2 = Math.max(ra2, ra1);
-        //console.log("random result; ", ra2);
-        
-        pDIF = ra2;
-  
-        // HIT CHANCE - Accuracy vs. Evasion
-        
-        var hitRate = 75; // percentile
-        hitRate += ((units[u1].stats['accuracy'] - units[u2].stats['evasion']) * 0.5);
-        
-        var lvlDiff = units[u2].lvl - units[u1].lvl;
-        
-        if (lvlDiff < 0)
-            lvlDiff = 0;
-        var accPenalty = (lvlDiff * 4);
-        hitRate -= (accPenalty * 0.5);
-        hitRate = Math.floor(hitRate);
-        
-        if (hitRate < 20)
-            hitRate = 20;
-        if (hitRate > 95)
-            hitRate = 95;
-        
-        
-        var hit = Math.random() * 100;
-        var evaded;
-        if (hit > hitRate)
-            evaded = true;
-        else
-            evaded = false;
-        
-        //console.log("pDIF: ", pDIF);
-        var damage = Math.floor(baseDamage * pDIF);
-
-        if (evaded)
-            return [-1, hitRate];
-        else
-            return [damage, hitRate];
-    }
 
     // Place each unit in the middle of their tile.
     function create_unitpos()
@@ -225,6 +124,14 @@ $(document).ready(function()
     {
         if (e.which === 3)
         {
+            var hover_unit = hover();
+            if (hover_unit !== -1)
+            {
+                //console.log(units[unit].name + " sets " + units[hover_unit].name + " id[" + hover_unit + "] as target.");
+                units[unit].targetUnit = hover_unit;
+            }
+            else
+                units[unit].targetUnit = -1;
             // Right-clicking initiates an attack.
             //actAttack(unit);
             /*
@@ -240,15 +147,17 @@ $(document).ready(function()
         }
         else
         {
-            var hover_unit = hover();
-            if (hover_unit !== -1)
+            var tileNotEmpty = false;
+            var xTile, yTile;
+            for (var i = 0; i < units.length; i++)
             {
-                //console.log("Clicked on unit ", hover_unit);
-                console.log(units[unit].name + " sets " + units[hover_unit].name + " id[" + hover_unit + "] as target.");
-                units[unit].targetUnit = hover_unit;
-                //Select_Unit(hover_unit);
-                //unit = hover_unit;
+                xTile = (unitpos[i][1] / tilesize) | 0;
+                yTile = (unitpos[i][0] / tilesize) | 0;
+                if (yTile === mouse_y_tile && xTile === mouse_x_tile)
+                    tileNotEmpty = true;
             }
+            if (tileNotEmpty)
+                console.log("Tile is occupied.");
             else
                 moveTo(unit, [mouse_y_tile, mouse_x_tile]);
                 //units[unit].targetUnit = -1;
@@ -282,16 +191,14 @@ $(document).ready(function()
         }
     }
 
-    $("canvas").mousemove(function (e)
+    function updateMouse()
     {
         var success = true;
 
         var rect = context.canvas.getBoundingClientRect();
-        var midx = screenX / 2;
-        var midy = screenY / 2;
 
-        mouse_x = (e.pageX - rect.left + unitpos[unit][1] - midx) | 0;
-        mouse_y = (e.pageY - rect.top + unitpos[unit][0] - midy) | 0;
+        mouse_x = (pMouse_x - rect.left + unitpos[unit][1] - midx) | 0;
+        mouse_y = (pMouse_y - rect.top + unitpos[unit][0] - midy) | 0;
         if (mouse_x < 0 || mouse_y < 0)
             success = false;
 
@@ -308,7 +215,13 @@ $(document).ready(function()
             mouse_x_tile = (mouse_x / tilesize) | 0;
             mouse_y_tile = (mouse_y / tilesize) | 0;
         }
-        
+    }
+
+
+    $("canvas").mousemove(function (e)
+    {
+        pMouse_x = e.pageX;
+        pMouse_y = e.pageY;
     });
 
    function draw_unitinfo(u)
@@ -319,17 +232,17 @@ $(document).ready(function()
         str += " " + jobnames[units[u].job].toUpperCase();
 
         //$("#unitname").text(str);
-        /*
+        
         str += ", Exp " + units[u].exp + "<br><br>";
         str += "hp " + units[u].maxhp + "/" + units[u].hp + "<br>";
         str += "mp " + units[u].maxmp + "/" + units[u].mp + "<br><br>";
-        str += "str: " + units[u].stats['str'] + ", dex: " + units[u].stats['dex'] + ", vit: " + units[u].stats['vit'] + "<br>";
-        str = str + "agi: " + units[u].stats['agi'] + ", int: " + units[u].stats['intl'] + ", mnd: " + units[u].stats['mnd'];
-        str = str + ", chr: " + units[u].stats['chr'];
+        str += "str: " + units[u].str + ", dex: " + units[u].dex + ", vit: " + units[u].vit + "<br>";
+        str = str + "agi: " + units[u].agi + ", int: " + units[u].int + ", mnd: " + units[u].mnd;
+        str = str + ", chr: " + units[u].chr;
 
         str = str + "<br><br>";
-        str = str + "attack: " + units[u].stats['attack'] + ", defense: " + units[u].stats['defense'] + "<br>";
-        str = str + "accuracy: " + units[u].stats['accuracy'] + ", evasion: " + units[u].stats['evasion'];
+        str = str + "attack: " + units[u].attack + ", defense: " + units[u].defense + "<br>";
+        str = str + "accuracy: " + units[u].accuracy + ", evasion: " + units[u].evasion;
         
         str += "<br><br>";
         str += "head...: ---<br>";
@@ -339,9 +252,10 @@ $(document).ready(function()
         str += "feet...: ---<br>";
         str += "rring..: ---<br>";
         str += "lring..: ---<br><br>";
-        */
+        
         str += "<br>atk cd: " + units[u].attackCooldownCur + "/" + units[0].attackCooldownMax + "<br>";
-        str += "target_id: " + units[u].targetUnit;
+        if (units[u].targetUnit !== -1)
+            str += "target_id: " + units[u].targetUnit;
 
         $("#unitinfo").html(str);
     }
@@ -349,33 +263,25 @@ $(document).ready(function()
     function placeUnits()
     {
         // Place units and enemies on valid tiles.
-        var isGood = 1;
+        var isGood;
         for (var i = 0; i < units.length; i++)
         {
-            var safety = 0;
             unitpos[i] = [];
             do
             {
                 isGood = 1;
-
                 // 40 is hardcoded. Remember, it must be less than worldsize.
                 unitpos[i][1] = ((Math.random() * 40) + 4) | 0;
                 unitpos[i][0] = ((Math.random() * 40) + 4) | 0;
 
                 if (map[ unitpos[i][0] ][ unitpos[i][1] ] !== 0)
                     isGood = -1;
+                else
+                    for (var j = 0; j < i; j++)
+                        if (unitpos[i][1] === unitpos[j][1] && unitpos[i][0] === unitpos[j][0])
+                            isGood = -1;
 
-                for (var j = 0; j < i; j++)
-                {
-                    if (unitpos[i][1] === unitpos[j][1] && unitpos[i][0] === unitpos[j][0])
-                    {
-                        isGood = -1;
-                        console.log("found dupe between " + (i + 1), (j + 1));
-                    }
-                }
-
-                safety = safety + 1;
-            } while (isGood === -1 && safety < 10);
+            } while (isGood === -1);
         }
 
         context.textAlign = "center";
@@ -383,9 +289,9 @@ $(document).ready(function()
 
         for (var i = 0; i < units.length; i++)
         {
-            units[i].stats = {str: 0, dex: 0, vit: 0, agi: 0, intl: 0, mnd: 0, chr: 0};
-            units[i].vitals = {maxhp: 0, hp: 0, maxmp: 0, mp: 0};
-        calc_stats(i);
+            //units[i].stats = {str: 0, dex: 0, vit: 0, agi: 0, intl: 0, mnd: 0, chr: 0};
+            //units[i].vitals = {maxhp: 0, hp: 0, maxmp: 0, mp: 0};
+            calc_stats(i);
         }
         create_unitpos();
     }
@@ -520,6 +426,7 @@ $(document).ready(function()
         var posy_tile = (unitpos[unit][0] / tilesize) | 0;
 
         update_movement();
+        updateMouse();
 
         // Clear the canvas
         context.clearRect(0, 0, context.canvas.width, context.canvas.height);
@@ -551,7 +458,13 @@ $(document).ready(function()
                 drawx = (x * tilesize) + midx - posx;
                 drawy = (y * tilesize) + midy - posy;
                 if (map[y][x] > 0)
+                {
+                    if (map[y][x] === 1)
+                        context.fillStyle = "#bbbbbb";
+                    else
+                        context.fillStyle = "#aa9999";
                     context.fillRect(drawx, drawy, tilesize, tilesize);
+                }
             }
         }
         
@@ -726,9 +639,6 @@ $(document).ready(function()
         });
     });
     */
-    window.setInterval(function ()
-    {
-    }, 8000);
 
     function attack(u1, u2)
     {
